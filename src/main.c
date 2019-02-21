e48e56498f18cd191550fb2890992a22c8452f45
suve <veg@svgames.pl>
2019-02-21 22:01:32 +0100

    Grab the title of the active window

--- src/main.c
+++ src/main.c
@@ -17,17 +17,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 
 #define APP_NAME    "taymtrakka"
 #define APP_VERSION "v.0.1"
 
+Window getActiveWindow(Display *display) {
+	Atom property = XInternAtom(display, "_NET_ACTIVE_WINDOW", False), type;
+	int form;
+	unsigned long remain, len;
+	unsigned char *list;
+
+	int status = XGetWindowProperty(
+		display,
+		XDefaultRootWindow(display),
+		property,
+		0,1024,
+		False,
+		33,
+		&type,
+		&form,
+		&len,
+		&remain,
+		&list
+	);
+	if(status != Success) {
+		return 0;
+	}
+	if(len == 0) {
+		return 0;
+	}
+
+	return ((Window*)list)[0];
+}
+
+char *getWindowName(Display *display, Window window) {
+	Atom property = XInternAtom(display, "WM_NAME", False), type;
+	int form;
+	unsigned long remain, len;
+	unsigned char *list;
+
+	int status = XGetWindowProperty(
+		display,
+		window,
+		property,
+		0, 1024,
+		False,
+		AnyPropertyType,
+		&type,
+		&form,
+		&len,
+		&remain,
+		&list
+	);
+	if(status != Success) {
+		return NULL;
+	}
+
+	return (char*)list;
+}
+
 int main(void) {
-	Display *disp = XOpenDisplay(NULL);
-	if(disp == NULL) {
+	Display *display = XOpenDisplay(NULL);
+	if(display == NULL) {
 		fprintf(stderr, APP_NAME ": Failed to open X11 display\n");
 		exit(EXIT_FAILURE);
 	}
 
-	XCloseDisplay(disp);
+	Window focused = getActiveWindow(display);
+	printf("Currently focused window: \"%s\"\n", getWindowName(display, focused));
+
+	XCloseDisplay(display);
 	return 0;
 }
