830dcfa70496a5b6bea894ae1343a649aafec2b9
suve <veg@svgames.pl>
2019-03-02 18:30:18 +0100

    Reuse sqlite3_stmt pointers instead of parsing SQL each time

--- src/db.c
+++ src/db.c
@@ -21,118 +21,117 @@
 #include "db.h"
 #include "os.h"
 
-static sqlite3 *datab = NULL;
+static struct {
+	sqlite3 *handle;
+	sqlite3_stmt *insert_window, *select_window;
+} db;
 
 int db_init(void) {
 	sqlite3_stmt *query;
-	
-	const char *sql = 
+
+	const char *sql =
 		#include "sql/db-init.c"
 	;
-	
+
 	do {
-		int err = sqlite3_prepare_v2(datab, sql, -1, &query, &sql);
+		int err = sqlite3_prepare_v2(db.handle, sql, -1, &query, &sql);
 		if(err != SQLITE_OK) {
 			const char *errmsg = sqlite3_errstr(err);
 			fprintf(stderr, "Failed to parse query: %s\n", errmsg);
-			
+
 			sqlite3_finalize(query);
 			return -1;
 		}
-		
+
 		err = sqlite3_step(query);
 		if(err != SQLITE_DONE) {
 			const char *errmsg = sqlite3_errstr(err);
 			fprintf(stderr, "Failed to execute query: %s\n", errmsg);
-			
+
 			sqlite3_finalize(query);
 			return -1;
 		}
-		
+
 		sqlite3_finalize(query);
 	} while(strlen(sql) > 0);
-	
+
 	return 0;
 }
 
 static int insert_window(const char *const windowName, sqlite3_int64 *rowID) {
-	sqlite3_stmt *query;
-	
-	int err = sqlite3_prepare_v2(datab,
-		#include "sql/insert-window.c"
-		, -1,
-		&query,
-		NULL
-	);
-	if(err != SQLITE_OK) {
-		const char *errmsg = sqlite3_errstr(err);
-		fprintf(stderr, "Failed to parse query: %s\n", errmsg);
-		
-		sqlite3_finalize(query);
-		return -1;
+	int err;
+	if(db.insert_window == NULL) {
+		err = sqlite3_prepare_v2(db.handle,
+			#include "sql/insert-window.c"
+			, -1,
+			&db.insert_window,
+			NULL
+		);
+		if(err != SQLITE_OK) {
+			const char *errmsg = sqlite3_errstr(err);
+			fprintf(stderr, "Failed to parse query: %s\n", errmsg);
+
+			sqlite3_finalize(db.insert_window);
+			db.insert_window = NULL;
+			return -1;
+		}
 	}
-	
-	err = sqlite3_bind_text(query, 1, windowName, -1, SQLITE_STATIC);
+	sqlite3_reset(db.insert_window);
+
+	err = sqlite3_bind_text(db.insert_window, 1, windowName, -1, SQLITE_STATIC);
 	if(err != SQLITE_OK) {
 		const char *errmsg = sqlite3_errstr(err);
 		fprintf(stderr, "Failed to bind query parameter: %s\n", errmsg);
-		
-		sqlite3_finalize(query);
 		return -1;
 	}
-	
-	err = sqlite3_step(query);
+
+	err = sqlite3_step(db.insert_window);
 	if(err != SQLITE_DONE) {
 		const char *errmsg = sqlite3_errstr(err);
 		fprintf(stderr, "Failed to execute query: %s\n", errmsg);
-		
-		sqlite3_finalize(query);
 		return -1;
 	}
-	
-	*rowID = sqlite3_last_insert_rowid(datab);
-	sqlite3_finalize(query);
+
+	*rowID = sqlite3_last_insert_rowid(db.handle);
 	return 0;
 }
 
 static int select_window(const char *const windowName, sqlite3_int64 *rowID) {
-	sqlite3_stmt *query;
-	
-	int err = sqlite3_prepare_v2(datab,
-		#include "sql/select-window.c"
-		, -1,
-		&query,
-		NULL
-	);
-	if(err != SQLITE_OK) {
-		const char *errmsg = sqlite3_errstr(err);
-		fprintf(stderr, "Failed to parse query: %s\n", errmsg);
-		
-		sqlite3_finalize(query);
-		return -1;
+	int err;
+	if(db.select_window == NULL) {
+		int err = sqlite3_prepare_v2(db.handle,
+			#include "sql/select-window.c"
+			, -1,
+			&db.select_window,
+			NULL
+		);
+		if(err != SQLITE_OK) {
+			const char *errmsg = sqlite3_errstr(err);
+			fprintf(stderr, "Failed to parse query: %s\n", errmsg);
+
+			sqlite3_finalize(db.select_window);
+			db.select_window = NULL;
+			return -1;
+		}
 	}
-	
-	err = sqlite3_bind_text(query, 1, windowName, -1, SQLITE_STATIC);
+	sqlite3_reset(db.select_window);
+
+	err = sqlite3_bind_text(db.select_window, 1, windowName, -1, SQLITE_STATIC);
 	if(err != SQLITE_OK) {
 		const char *errmsg = sqlite3_errstr(err);
 		fprintf(stderr, "Failed to bind query parameter: %s\n", errmsg);
-		
-		sqlite3_finalize(query);
 		return -1;
 	}
-	
-	err = sqlite3_step(query);
+
+	err = sqlite3_step(db.select_window);
 	if(err == SQLITE_ROW) {
-		*rowID = sqlite3_column_int64(query, 0);
-		sqlite3_finalize(query);
+		*rowID = sqlite3_column_int64(db.select_window, 0);
 		return 0;
 	} else if(err == SQLITE_DONE) {
 		return insert_window(windowName, rowID);
 	} else {
 		const char *errmsg = sqlite3_errstr(err);
 		fprintf(stderr, "Failed to execute query: %s\n", errmsg);
-		
-		sqlite3_finalize(query);
 		return -1;
 	}
 }
@@ -140,44 +139,47 @@ static int select_window(const char *const windowName, sqlite3_int64 *rowID) {
 int db_add(const char *const windowName) {
 	sqlite3_int64 rowID;
 	int err = select_window(windowName, &rowID);
-	
+
 	if(err == 0) fprintf(stderr,"--> window \"%s\" has DB ID #%llu\n", windowName, rowID);
-	
+
 	return 0;
 }
 
 int db_open(void) {
 	// Check if db is already open
-	if(datab != NULL) return 0;
-	
+	if(db.handle != NULL) return 0;
+
 	char buffer[1024];
 	os_get_db_path(buffer, sizeof(buffer));
-	
+
 	// TODO: Check the function's exit code for more detailed error info.
-	int err = sqlite3_open_v2(buffer, &datab, SQLITE_OPEN_READWRITE, NULL);
+	int err = sqlite3_open_v2(buffer, &db.handle, SQLITE_OPEN_READWRITE, NULL);
 	if(err == SQLITE_OK) return 0;
-	
-	sqlite3_close(datab);
-	datab = NULL;
-	
+
+	sqlite3_close(db.handle);
+	db.handle = NULL;
+
 	char *slash = strrchr(buffer, '/');
 	*slash = '\0';
 	if(os_mkdir(buffer) != 0) return -1;
-	
+
 	*slash = '/';
-	err = sqlite3_open_v2(buffer, &datab, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+	err = sqlite3_open_v2(buffer, &db.handle, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
 	if(err != SQLITE_OK) {
 		const char *errmsg = sqlite3_errstr(err);
-		fprintf(stderr, "Failed to open database: %s\n", errmsg);
-		
+		fprintf(stderr, "Failed to open db.handlease: %s\n", errmsg);
+
 		return -1;
 	}
-	
+
 	return 0;
 }
 
 int db_close(void) {
-	sqlite3_close(datab);
-	datab = NULL;
+	sqlite3_finalize(db.insert_window);
+	sqlite3_finalize(db.select_window);
+
+	sqlite3_close(db.handle);
+	db.handle = NULL;
 	return 0;
 }
